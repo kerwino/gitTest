4a455494b873c7855eb76d4599350e4988640717
kerwino <1746804529@qq.com>
2021-03-15 20:03:16 +0800

    Initial Commit

---
+++ gitTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gitTest
+//
+//  Created by kerwino on 2021/3/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
